aee09d62b6408c527aea1f4c5cc758c9a3b2a13b
JD Porterfield <jdvporterfield@gmail.com>
2018-06-10 12:43:57 -0400

    Initial addition of the home view

--- app/src/main/java/me/jdvp/weatherappjava/MainActivity.java
+++ app/src/main/java/me/jdvp/weatherappjava/MainActivity.java
@@ -1,13 +1,32 @@
 package me.jdvp.weatherappjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.current_weather_image_view)
+    ImageView currentWeatherImageView;
+
+    @BindView(R.id.current_weather_text_view)
+    TextView currentWeatherTextView;
+
+    @BindView(R.id.current_temperature_text_view)
+    TextView currentTemperatureTextView;
+
+    @BindView(R.id.hourly_forecast_recycler_view)
+    RecyclerView hourlyForecastRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
